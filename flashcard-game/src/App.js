59fd0bb9fb6b3eb049b891dc093894730cc88cc4
farjanaHuq <fa.huq03@gmail.com>
2022-04-21 12:17:06 -0700

    Form added to select category

--- flashcard-game/src/App.js
+++ flashcard-game/src/App.js
@@ -1,34 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import FlashcardList from "./component/FlashcardList";
 import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [flashcards, setFlashCards] = useState(SAMPLE_FLASHCARDS);
-  useEffect(() => {
-    axios
-      .get(
-        "https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple"
-      )
-      .then((res) => {
-        setFlashCards(
-          res.data.results.map((questionItem, index) => {
-            const answer = questionItem.correct_answer; //we want our question to be completely in random order, to do so we create another variable called option.
-            const options = [
-              ...questionItem.incorrect_answers.map((a) => decodeString(a)),
-              answer,
-            ];
-            return {
-              id: `${index}-${Date.now()}`, //to ensure it's actually be unique
-              questionItem: questionItem.quetion,
-              answer: answer,
-              options: options.sort(() => Math.random() - 0.5), //50% time we get a positive number and 50% time gives negetive numbers
-            };
-          })
-        );
-        console.log(res.data);
-      });
-  }, []);
+  const [flashcards, setFlashCards] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const categoryEl = useRef();
+  const amountEl = useRef();
+
+  //get category from API
+  useEffect(() =>{
+    axios.get('https://opentdb.com/api_category.php')
+    .then(res =>{
+       setCategories(res.data.trivia_categories);
+    })
+  })
+
+  // useEffect(() => {
+    // axios
+    //   .get(
+    //     "https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple"
+    //   )
+    //   .then((res) => {
+    //     setFlashCards(
+    //       res.data.results.map((questionItem, index) => {
+    //         const answer = questionItem.correct_answer; //we want our question to be completely in random order, to do so we create another variable called option.
+    //         const options = [
+    //           ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+    //           answer,
+    //         ];
+    //         return {
+    //           id: `${index}-${Date.now()}`, //to ensure it's actually be unique
+    //           questionItem: questionItem.quetion,
+    //           answer: answer,
+    //           options: options.sort(() => Math.random() - 0.5), //50% time we get a positive number and 50% time gives negetive numbers
+    //         };
+    //       })
+    //     );
+    //     console.log(res.data);
+    //   });
+  // }, []);
 
   //Decode string
   function decodeString(str) {
@@ -36,26 +49,77 @@ function App() {
     textArea.innerHTML = str;
     return textArea.value;
   }
+  function handleSubmit(e){
+    e.preventDefault();
+    axios
+    .get(
+      "https://opentdb.com/api.php",{
+        params: {
+          amountEl: amountEl.current.value,
+          categoryEl: categoryEl.current.value
+        } 
+      }
+      
+    )
+    .then((res) => {
+      setFlashCards(
+        res.data.results.map((questionItem, index) => {
+          const answer = questionItem.correct_answer; //we want our question to be completely in random order, to do so we create another variable called option.
+          const options = [
+            ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+            answer,
+          ];
+          return {
+            id: `${index}-${Date.now()}`, //to ensure it's actually be unique
+            questionItem: questionItem.quetion,
+            answer: answer,
+            options: options.sort(() => Math.random() - 0.5), //50% time we get a positive number and 50% time gives negetive numbers
+          };
+        })
+      );
+     // console.log(res.data);
+    });
+  }
 
   return (
-    <div className="container">
-      <h1>Hello World</h1>
+    <>
+    <form className="header" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="category">Category</label>
+        <select id="category" ref={categoryEl}>
+           {categories.map(category =>{
+              return <option value={category.id} key={category.id}>{category.name}</option>
+           })}
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="amount">Number of Questions</label>
+        <input type="number" id="amount" min={1} step={1} defaultValue={10} ref={amountEl}></input>
+      </div>
+      <div className="form-group">
+          <button className="btn">Generate</button>
+      </div>
+    </form>
+
+     <div className="container">
       <FlashcardList flashcards={flashcards} />
     </div>
+    </>
+   
   );
 }
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    quetion: "what is 2+2",
-    answer: "4",
-    options: ["2", "4", "5"],
-  },
-  {
-    id: 2,
-    quetion: "what is getorade?",
-    answer: "drink",
-    options: ["animal", "mammal", "drink"],
-  },
-];
+// const SAMPLE_FLASHCARDS = [
+//   {
+//     id: 1,
+//     question: "what is 2+2?",
+//     answer: "4",
+//     options: ["2", "4", "5"],
+//   },
+//   {
+//     id: 2,
+//     quetion: "what is getorade?",
+//     answer: "drink",
+//     options: ["animal", "mammal", "drink"],
+//   },
+// ];
 export default App;
--- flashcard-game/src/component/Flashcard.js
+++ flashcard-game/src/component/Flashcard.js
@@ -1,15 +1,35 @@
-// import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-import { useState } from "react";
-
-
-
+//useRef : allows us to get the reference of different element of our page
 export default function Flashcard({ flashcard }) {
 
   const [flip, setFlip] = useState(false);
+  const [height, setHeight] = useState('initial');
+  const frontEl = useRef(); //refers to current front Element of the card
+  const backEl = useRef();
+  
+  //Set the height of the flashcard element dynamically
+  function setMaxHeight(){
+    const frontHeight = frontEl.current.getBoundingClientRect().height; //refers to current front Element of the card
+    const backHeight = backEl.current.getBoundingClientRect().height;
+    setHeight(Math.max(frontHeight, backHeight, 100));  //setting a default minimum (100) and max height and passing through setHeight function
+  }
+  
+  useEffect(setMaxHeight,
+  [flashcard.question, flashcard.answer, flashcard.options]); //anytime these element changes we are going to recalculate the height
+
+  //recalculate height everytime the page size changes
+  useEffect(()=>{
+    window.addEventListener('resize', setMaxHeight);
+    return() => window.removeEventListener('resize', setMaxHeight);
+  }, [])
+
   return (
-   <div className= {`card ${flip? 'flip': ''}`}>
-    <div className="front">
+   <div className= {`card ${flip? 'flip': ''}`}
+      style= {{height: height}}
+      onClick = {() => setFlip(!flip)}
+   >
+    <div className="front" ref={frontEl}>
       {flashcard.question}
       <div className="flashcard-options">
         {flashcard.options.map((option,id) =>{
@@ -17,7 +37,7 @@ export default function Flashcard({ flashcard }) {
         })}
       </div>
     </div>
-    <div className="back">{flashcard.answer}</div>
+    <div className="back" ref={backEl}>{flashcard.answer}</div>
     </div>
   )  
 }
